b19e896251595c527f7d5aa8cd2d98a4496728e9
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-07-07 00:13:55 -0700

    display list of animals

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import DisplayListAnimals from './DisplayListAnimals';
+
 class Dashboard extends Component {
   constructor(props) {
     super(props);
@@ -24,8 +26,6 @@ class Dashboard extends Component {
   handleSubmit(event) {
     const { zipcode, animal } = this.state;
 
-    console.log(`${zipcode} ${animal}`);
-
     fetch(`/petbasicfind/${animal}/${zipcode}`)
       .then(res => res.json())
       .then(data => this.setState({ animals: data.petfinder.pets.pet }));
@@ -34,41 +34,48 @@ class Dashboard extends Component {
   }
 
   render() {
-    const { value } = this.state;
+    const { value, animals } = this.state;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input placeholder="Zip Code" type="number" value={value} onChange={this.handleZipcode} />
-        <br />
-        <br />
-        Pick an animal:
-        <br />
-        <select value={value} onChange={this.handleAnimal}>
-          <option value="dog">
-            Dog
-          </option>
-          <option value="cat">
-            Cat
-          </option>
-          <option value="bird">
-            Bird
-          </option>
-          <option value="reptile">
-            Reptile
-          </option>
-          <option value="horse">
-            Horse
-          </option>
-          <option value="barnyard">
-            Barnyard
-          </option>
-          <option value="smallfurry">
-            Smallfurry
-          </option>
-        </select>
-        <br />
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input placeholder="Zip Code" type="number" value={value} onChange={this.handleZipcode} />
+            <br />
+            <br />
+            Pick an animal:
+            <br />
+            <select value={value} onChange={this.handleAnimal}>
+              <option value="dog">
+                Dog
+              </option>
+              <option value="cat">
+                Cat
+              </option>
+              <option value="bird">
+                Bird
+              </option>
+              <option value="reptile">
+                Reptile
+              </option>
+              <option value="horse">
+                Horse
+              </option>
+              <option value="barnyard">
+                Barnyard
+              </option>
+              <option value="smallfurry">
+                Smallfurry
+              </option>
+            </select>
+            <br />
+            <br />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+        <div>
+          {animals.map(x => <DisplayListAnimals singleAnimal={x} key={x.id.$t} />)}
+        </div>
+      </div>
     );
   }
 }
