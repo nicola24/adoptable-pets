8d67affb166b0b768fde96d6970bbebc2125e56d
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-07-09 15:34:15 -0700

    singleAnimal view component done, all data display

--- src/SingleAnimal.jsx
+++ src/SingleAnimal.jsx
@@ -9,42 +9,45 @@ const SingleAnimal = ({ singleAnimalDisplay }) => {
     ? singleAnimalDisplay[0].breeds.breed.map(x => Object.values(x)).join(' & ')
     : singleAnimalDisplay[0].breeds.breed.$t;
 
+  const animalOptions = Array.isArray(singleAnimalDisplay[0].options.option)
+    ? singleAnimalDisplay[0].options.option.map(x => Object.values(x)).join(', ')
+    : null;
+
   return (
     <div>
-      <ul>
-        <li>
-          <img src={filterImgList} alt="" />
-          <h3>
-            {singleAnimalDisplay[0].name.$t}
-          </h3>
-          <h3>
-            {singleAnimalDisplay[0].age.$t}
-          </h3>
-          <h3>
-            {singleAnimalDisplay[0].sex.$t === 'M' ? 'Male' : 'Female'}
-          </h3>
-          <h3>
-            {breedList}
-          </h3>
-          <h4>
-            {singleAnimalDisplay[0].description.$t}
-          </h4>
-          <h4>
-            {singleAnimalDisplay[0].contact.address1.$t}
-          </h4>
-          <h4>
-            {singleAnimalDisplay[0].contact.city.$t}
-            {singleAnimalDisplay[0].contact.state.$t}
-            {singleAnimalDisplay[0].contact.zip.$t}
-          </h4>
-          <h4>
-            {singleAnimalDisplay[0].contact.phone.$t}
-          </h4>
-          {/* <a href="mailto:" + {singleAnimalDisplay[0].contact.email.$t}>
-            {singleAnimalDisplay[0].contact.email.$t}
-          </a> */}
-        </li>
-      </ul>
+      <img src={filterImgList} alt="" />
+      <h3>
+        {singleAnimalDisplay[0].name.$t}
+      </h3>
+      <h3>
+        {singleAnimalDisplay[0].age.$t}
+      </h3>
+      <h3>
+        {singleAnimalDisplay[0].sex.$t === 'M' ? 'Male' : 'Female'}
+      </h3>
+      <h3>
+        {breedList}
+      </h3>
+      <h3>
+        {animalOptions}
+      </h3>
+      <h4>
+        {singleAnimalDisplay[0].description.$t}
+      </h4>
+      <h4>
+        {singleAnimalDisplay[0].contact.address1.$t}
+      </h4>
+      <h4>
+        {singleAnimalDisplay[0].contact.city.$t}
+        {singleAnimalDisplay[0].contact.state.$t}
+        {singleAnimalDisplay[0].contact.zip.$t}
+      </h4>
+      <h4>
+        {singleAnimalDisplay[0].contact.phone.$t}
+      </h4>
+      <a href={`mailto:${singleAnimalDisplay[0].contact.email.$t}`}>
+        {singleAnimalDisplay[0].contact.email.$t}
+      </a>
     </div>
   );
 };
