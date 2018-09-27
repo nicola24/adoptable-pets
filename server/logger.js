9b41fbae7c22f14ae0d0e0662c6edd785e6f9c77
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-09-26 19:00:30 -0700

    added webpack prob/dev

--- server/logger.js
+++ server/logger.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 
-const divider = chalk.gray('\n-----------------------------------');
+const divider = chalk.gray('\n----------------------------------------');
 
 const logger = {
   error: err => console.error(chalk.red(err)),
@@ -9,6 +9,7 @@ const logger = {
     Server started ! ${chalk.green('✓')} \n
     ${chalk.bold('Access URLs:')}${divider}
     Localhost: ${chalk.magenta(`http://localhost:${port}`)}${divider}
+    ${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
     `),
 };
 
